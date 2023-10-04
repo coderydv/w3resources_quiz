da269b1d2e47d13d048b6cf7e4d9833c8fa25e62
coderydv <89965195+coderydv@users.noreply.github.com>
2023-10-04 20:42:45 +0530

    New file for big c and f

---
+++ bigfandc.c
@@ -0,0 +1,25 @@
+#include<stdio.h> 
+
+int main(){
+    //In this code we are printing the Big F and C using #
+
+    printf(" Now we are printing Big F\n");
+    printf("########\n");
+    printf("##\n");
+    printf("##\n");
+    printf("########\n");
+    printf("##\n");
+    printf("##\n");
+    printf("##\n");
+
+pritf("Now we are printing Big c ");
+
+printf("    #####\n");
+printf("####     ##\n");
+printf("###\n");
+printf("##\n");
+printf("###\n");
+printf("####     ##\n");
+printf("    #####\n");
+return 0;
+}

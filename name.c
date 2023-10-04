83ff53464928bb8da587603a913d7bf4b1a1bca0
coderydv <mohitydvv4@gmail.com>
2023-10-04 20:51:38 +0530

    name and dob display

---
+++ name.c
@@ -0,0 +1,8 @@
+#include<stdio.h> 
+
+int main(){
+    printf(" My name is Mohit yadav\n");
+    printf(" My Date of Birth is 30 December 1998\n");
+    printf(" This is my first clone repository on github\n");
+return 0;
+}
